bb5d7c82ed5ff8efa763332ee1d67188333bdb5d
Cassy Dalil <cassydalil@gmail.com>
2022-06-01 20:45:18 -0400

    more edits in js

--- script.js
+++ script.js
@@ -9,23 +9,26 @@ let downArrow5 = document.querySelector("#down-arrow5");
 
 function showAnswer1() {
     document.getElementById("answer-1").classList.toggle("show");
-    // document.getElementById("question-1").style.fontWeight = "700";
-    downArrow1.style.transform = 'rotate(180deg)';
     document.getElementById("answer-2").classList.remove("show");
     document.getElementById("answer-3").classList.remove("show");
     document.getElementById("answer-4").classList.remove("show");
     document.getElementById("answer-5").classList.remove("show");
+    downArrow1.style.transform = 'rotate(180deg)';
     downArrow2.style.transform = 'rotate(360deg)';
     downArrow3.style.transform = 'rotate(360deg)';
     downArrow4.style.transform = 'rotate(360deg)';
     downArrow5.style.transform = 'rotate(360deg)';
+    document.getElementById("question-1").style.fontWeight = "700";
+    document.getElementById("question-2").style.fontWeight = "500";
+    document.getElementById("question-3").style.fontWeight = "500";
+    document.getElementById("question-4").style.fontWeight = "500";
+    document.getElementById("question-5").style.fontWeight = "500";
 }
 
 downArrow1.addEventListener("click", showAnswer1);
 
 function showAnswer2() {
     document.getElementById("answer-2").classList.toggle("show");
-    // document.getElementById("question-2").style.fontWeight = "800";
     document.getElementById("answer-1").classList.remove("show");
     document.getElementById("answer-3").classList.remove("show");
     document.getElementById("answer-4").classList.remove("show");
@@ -35,13 +38,17 @@ function showAnswer2() {
     downArrow3.style.transform = 'rotate(360deg)';
     downArrow4.style.transform = 'rotate(360deg)';
     downArrow5.style.transform = 'rotate(360deg)';
+    document.getElementById("question-1").style.fontWeight = "500";
+    document.getElementById("question-2").style.fontWeight = "700";
+    document.getElementById("question-3").style.fontWeight = "500";
+    document.getElementById("question-4").style.fontWeight = "500";
+    document.getElementById("question-5").style.fontWeight = "500";
 }
 
 downArrow2.addEventListener("click", showAnswer2);
 
 function showAnswer3() {
     document.getElementById("answer-3").classList.toggle("show");
-    // document.getElementById("question-3").style.fontWeight = "800";
     document.getElementById("answer-2").classList.remove("show");
     document.getElementById("answer-1").classList.remove("show");
     document.getElementById("answer-4").classList.remove("show");
@@ -51,13 +58,17 @@ function showAnswer3() {
     downArrow1.style.transform = 'rotate(360deg)';
     downArrow4.style.transform = 'rotate(360deg)';
     downArrow5.style.transform = 'rotate(360deg)';
+    document.getElementById("question-1").style.fontWeight = "500";
+    document.getElementById("question-2").style.fontWeight = "500";
+    document.getElementById("question-3").style.fontWeight = "700";
+    document.getElementById("question-4").style.fontWeight = "500";
+    document.getElementById("question-5").style.fontWeight = "500";
 }
 
 downArrow3.addEventListener("click", showAnswer3);
 
 function showAnswer4() {
     document.getElementById("answer-4").classList.toggle("show");
-    // document.getElementById("question-4").style.fontWeight = "800";
     document.getElementById("answer-2").classList.remove("show");
     document.getElementById("answer-3").classList.remove("show");
     document.getElementById("answer-1").classList.remove("show");
@@ -67,13 +78,17 @@ function showAnswer4() {
     downArrow3.style.transform = 'rotate(360deg)';
     downArrow1.style.transform = 'rotate(360deg)';
     downArrow5.style.transform = 'rotate(360deg)';
+    document.getElementById("question-1").style.fontWeight = "500";
+    document.getElementById("question-2").style.fontWeight = "500";
+    document.getElementById("question-3").style.fontWeight = "500";
+    document.getElementById("question-4").style.fontWeight = "700";
+    document.getElementById("question-5").style.fontWeight = "500";
 }
 
 downArrow4.addEventListener("click", showAnswer4);
 
 function showAnswer5() {
     document.getElementById("answer-5").classList.toggle("show");
-    // document.getElementById("question-5").style.fontWeight = "800";
     document.getElementById("answer-2").classList.remove("show");
     document.getElementById("answer-3").classList.remove("show");
     document.getElementById("answer-4").classList.remove("show");
@@ -83,6 +98,11 @@ function showAnswer5() {
     downArrow3.style.transform = 'rotate(360deg)';
     downArrow4.style.transform = 'rotate(360deg)';
     downArrow1.style.transform = 'rotate(360deg)';
+    document.getElementById("question-1").style.fontWeight = "500";
+    document.getElementById("question-2").style.fontWeight = "500";
+    document.getElementById("question-3").style.fontWeight = "500";
+    document.getElementById("question-4").style.fontWeight = "500";
+    document.getElementById("question-5").style.fontWeight = "700";
 }
 
 downArrow5.addEventListener("click", showAnswer5);
